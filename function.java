44cfa139492272f8d9df1b52d650b9d1e9f82a5b
Revanth-123456 <revanthvs04@gmail.com>
2026-01-29 15:30:24 +0530

    commit

---
+++ function.java
@@ -0,0 +1,9 @@
+// function is very important repetative,code reusability,money,readability
+// function syntax-returntype function-important name(list of parameters)
+
+public class function {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ variables.java
@@ -0,0 +1,23 @@
+// inside cpu many registers-it is memory side cache
+// ram where data comes temporary memory example 
+// why variables we need to write ram address it is very difficult
+// simplicity,readable,flexibility(dma),need to write correctly
+// datatype efficient use of ram,type specificorchecking,type casting,
+// 
+public class variables {
+    public static void main(String[] args) {
+        String name = "revu";
+        System.out.println("String:name " + name);
+
+        int age = 25;
+        System.out.println("Integer age " + age);
+
+        float height = 9.8f;
+        System.out.println("Float:height " + height);
+
+        boolean isActive = true;
+        System.out.println("boolean " + isActive);
+
+    }
+
+}
