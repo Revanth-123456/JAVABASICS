3e8e8d63dfa468512fd4bdd69ce89a369913ba62
Revanth-123456 <revanthvs04@gmail.com>
2026-01-30 13:48:21 +0530

    commit

--- function2.java
+++ function2.java
@@ -28,10 +28,10 @@
 */
 public class function2 {
     public static void main(String[] args) {
-        // String StudentName = getTopperNameForSubjectGrade(1, "Maths");
-        // System.out.println(StudentName);
+        String StudentName = getTopperNameForSubjectGrade(1, "Maths");
+        System.out.println(StudentName);
 
-        printStudentDetails(1);
+        // printStudentDetails(1);
     }
 
     // Who is the topper
@@ -40,32 +40,32 @@ public class function2 {
     // response name of the student
 
     static String getTopperNameForSubjectGrade(int grade, String subject) {
-        // Dummy implenmentation
+        // Dummy implenmentation'
         String name = "Sangeeta";
         return name;
     }
 
     // Returns array in which at 0 index boys count is present and 1 index girls
     // count is present
-    static int[] getTotalCountOfBoysGirls(int schoolID) {
-        // Dummy
-        int[] results = { 500, 500 };
-        return results;
-    }
+    // static int[] getTotalCountOfBoysGirls(int schoolID) {
+    // // Dummy
+    // int[] results = { 500, 500 };
+    // return results;
 
-    static void printStudentDetails(int studentId) {
-        // Dummy
-        String name = "Radha";
-        int age = 18;
-        int grade = 10;
-        double CGPA = 9.2;
-        boolean isRegularToSchool = true;
+    // }
+    // static void printStudentDetails(int studentId) {
+    // // Dummy
+    // String name = "Radha";
+    // int age = 18;
+    // int grade = 10;
+    // double CGPA = 9.2;
+    // boolean isRegularToSchool = true;
 
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(grade);
-        System.out.println(CGPA);
-        System.out.println(isRegularToSchool);
-    }
+    // System.out.println(name);
+    // System.out.println(age);
+    // System.out.println(grade);
+    // System.out.println(CGPA);
+    // System.out.println(isRegularToSchool);
+    // }
 
 }
